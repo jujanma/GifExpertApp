0ee29f80495edef3aa9a529e22ab7600038eb5dc
Juan Manuel Marin <juanchomanuel289@gmail.com>
2023-02-15 15:12:44 -0500

    clase 2

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { React, useState } from "react";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["one puta madre"]);
+
+  const onAddCategory = () => {
+    console.log("funcion");
+    setCategories(["Valorant", ...categories]);
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <button onClick={onAddCategory}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
