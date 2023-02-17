ff5fb947e4b188635a3a7921f10842cb9961380a
Juan Manuel Marin <juanchomanuel289@gmail.com>
2023-02-16 19:08:52 -0500

    clase 8

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+  useEffect(() => {
+    getImages();
+  }, []);
 
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {images.map((titulo) => (
+          <li key={titulo.id}>{titulo.title}</li>
+        ))}
+      </ol>
     </>
   );
 };
